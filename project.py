1cf41afb6ec24c6f9797d314eb17d2646afc2302
Hansraj61 <95349316+Hansraj61@users.noreply.github.com>
2024-07-08 16:06:45 +0530

    Update project.py

--- project.py
+++ project.py
@@ -22,13 +22,13 @@ except FileNotFoundError:
 except Exception as e:
     st.error(f"An error occurred while loading the model: {e}")
 
-SquareFeet = st.number_input("Enter the size of house", min_value=60, max_value=2400, step=50)    
-Bedrooms = st.number_input("Enter the no of bedrooms", min_value=0, max_value=4, step=1)    
+SquareFeet = st.number_input("Enter the size of house", min_value=900, max_value=3000, step=50)    
+Bedrooms = st.number_input("Enter the no of bedrooms", min_value=0, max_value=5, step=1)    
 Bathrooms = st.number_input("Enter the no of bathrooms", min_value=0, max_value=6, step=1)    
 Neighborhood = st.radio("Enter the neighborhood", ["Rural", "Urban", "Suburb"])
 neighbor = 1 if Neighborhood == "Rural" else 2 if Neighborhood == "Urban" else 3
 
-YearBuilt = st.number_input("Enter the number of year of construction", min_value=2000, max_value=2050, step=1)
+YearBuilt = st.number_input("Enter the number of year of construction", min_value=2023, max_value=2060, step=1)
 
 if st.button("Predict Price"):
     price = model.predict([[SquareFeet, Bedrooms, Bathrooms, neighbor, YearBuilt]])
